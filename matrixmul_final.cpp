ccb0ed96ae17b549350606da242276f6481c6978
cyy1916 <72010460+cyy1916@users.noreply.github.com>
2020-11-14 19:51:15 +0800

    Create matrixmul_final.cpp

---
+++ matrixmul_final.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <ctime>
+#include <chrono>
+#include <thread>
+#include<vector>
+#include <memory> 
+#include <immintrin.h>
+#include<omp.h>
+#include <thread>
+using namespace std;
+using namespace std::literals;
+struct matrixs
+{
+	int rownum;
+	int linenum;
+	float **ma;
+};
+
+matrixs dotmultiplication(float **a, float **b, int i, int j, int k)
+{
+	__m256 a1, b1;
+	__m256 c1 = _mm256_setzero_ps();
+	matrixs sum;
+	sum.linenum = i;
+	sum.rownum = k;
+	sum.ma = new float*[i];
+	for (int n = 0; n < i; n++)
+	{
+		sum.ma[n] = new float[k];
+	}
+	float s = 0;
+#pragma omp parallel for
+	for (int n = 0; n < i; n++)
+	{
+		for (int p = 0; p < j; p = p + 8)
+		{
+			a1 = _mm256_load_ps(a[n] + p);
+			for (int m = 0; m < k; m = m + 8)
+			{
+				b1 = _mm256_load_ps(b[p] + m);
+				c1 = _mm256_add_ps(c1, _mm256_mul_ps(a1, b1));
+			}
+		}
+		_mm256_store_ps(sum.ma[n], c1);
+	}
+	
+	return sum;
+}
+
+float* data(int n)
+{
+	float *a = new float[n + 1]{ 0 };
+	for (int i = 0; i < n; i++)
+	{
+		a[i] = rand() + rand() / float(RAND_MAX);
+	}
+	return a;
+}
+
+int main(int argc, const char * argv[])
+{
+	auto now = std::chrono::system_clock::now();
+	auto t_c = std::chrono::system_clock::to_time_t(now - 24h);
+	matrixs a;
+	matrixs b;
+	int n1 = 0;
+	int m1 = 0;
+	int n2 = 0;
+	int m2 = 0;
+	matrixs sum;
+	cin >> n1;
+	cin >> m1;
+	cin >> n2;
+	cin >> m2;
+	if (m1 != n2)
+	{
+		cout << "矩阵格式有误" << endl;
+	}
+	else
+	{
+		a.linenum = n1;
+		a.rownum = m1;
+		b.linenum = n2;
+		b.rownum = m2;
+		a.ma = new float*[n1 + 1];
+		for (int i = 0; i < n1; i++)
+		{
+			a.ma[i] = new float[m1 + 1];
+		}
+		b.ma = new float*[n2 + 1];
+		for (int i = 0; i < n2; i++)
+		{
+			b.ma[i] = new float[m2 + 1];
+		}
+		for (int i = 0; i < n1; i++)
+		{
+			srand((unsigned int)(time(NULL)));
+			a.ma[i] = data(m1);
+		}
+		for (int i = 0; i < n2; i++)
+		{
+			srand((unsigned int)(time(NULL)));
+			b.ma[i] = data(m2);
+		}
+		auto start1 = std::chrono::steady_clock::now();
+
+		sum = dotmultiplication(a.ma, b.ma, n1, m1, m2);
+		auto end1 = std::chrono::steady_clock::now();
+		std::cout
+			<< "计算时间(ikj)：  "
+			<< std::chrono::duration_cast<std::chrono::microseconds>(end1 - start1).count() << "us ≈ "
+			<< std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1).count() << "ms ≈ "
+			<< std::chrono::duration_cast<std::chrono::seconds>(end1 - start1).count() << "s.\n";
+		cout << endl;
+		cout << endl;
+		cout << endl;
+		for (int i = 0; i < n1; i++)
+		{
+			for (int j = 0; j < m2; j++)
+			{
+				cout << sum.ma[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+
+
+}
